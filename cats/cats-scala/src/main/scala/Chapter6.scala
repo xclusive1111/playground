611b1b67642cfeef3d8549686ea18a970db2ce68
xclusive1111 <son.nheo35@gmail.com>
2019-06-17 11:31:32 +0700

    Cats: Form validation using Semigroupal

--- cats/cats-scala/src/main/scala/Chapter6.scala
+++ cats/cats-scala/src/main/scala/Chapter6.scala
@@ -37,6 +37,15 @@ object Chapter6 {
       }
     }
 
+    implicit def eitherSemigroupal[E](implicit M: MyMonoid[E]): MySemigroupal[Either[E, ?]] = new MySemigroupal[Either[E, ?]] {
+      override def product[A, B](fa: Either[E, A], fb: Either[E, B]): Either[E, (A, B)] = (fa, fb) match {
+        case (Right(v1), Right(v2)) => Right((v1, v2))
+        case (Right(v1), Left(e1))  => Left(e1)
+        case (Left(e1), Right(v1))  => Left(e1)
+        case (Left(e1), Left(e2))   => Left(M.combine(e1, e2))
+      }
+    }
+
     implicit def validatedSemigroupal[E](implicit M: MyMonoid[E]): MySemigroupal[MyValidated[E, ?]] = new MySemigroupal[MyValidated[E, ?]] {
       override def product[A, B](fa: MyValidated[E, A], fb: MyValidated[E, B]): MyValidated[E, (A, B)] = (fa, fb) match {
         case (MyInvalid(e1), MyInvalid(e2)) => MyInvalid(M.combine(e1, e2))
@@ -51,6 +60,80 @@ object Chapter6 {
     implicit class ValidatedOps[A](value: A) {
       def valid[X]: MyValidated[X, A] = MyValid(value)
       def invalid[X]: MyValidated[A, X] = MyInvalid(value)
+    }
+
+    def tupled2[E, A, B](t2: (MyValidated[E, A], MyValidated[E, B]))
+                        (implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, (A, B)] =
+      S.product(t2._1, t2._2)
+
+    def map2[E, A, B, C](t2: (MyValidated[E, A], MyValidated[E, B]))
+                        (f: (A, B) => C)
+                        (implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, C] =
+      S.product(t2._1, t2._2) match {
+        case MyValid(value) => MyValid(f(value._1, value._2))
+        case MyInvalid(err) => MyInvalid(err)
+      }
+
+
+    def tupled3[E, A, B, C](t3: (MyValidated[E, A], MyValidated[E, B], MyValidated[E, C]))
+                           (implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, (A, B, C)] =
+      t3 match {
+        case (fa, fb, fc) =>
+          val fab = tupled2(fa, fb)
+          tupled2(fab, fc) match {
+            case MyValid(((a, b), c)) => MyValid((a, b, c))
+            case MyInvalid(e)         => MyInvalid(e)
+          }
+      }
+
+    def map3[E, A, B, C, D](t3: (MyValidated[E, A], MyValidated[E, B], MyValidated[E, C]))
+                           (f: (A, B, C) => D)
+                           (implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, D] =
+      tupled3(t3) match {
+        case MyValid(value) => MyValid(f(value._1, value._2, value._3))
+        case MyInvalid(err) => MyInvalid(err)
+      }
+
+
+    def tupled4[E, A, B, C, D](t4: (MyValidated[E, A], MyValidated[E, B], MyValidated[E, C], MyValidated[E, D]))
+                              (implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, (A, B, C, D)] =
+      t4 match {
+        case (fa, fb, fc, fd) =>
+          val fabc = tupled3(fa, fb, fc)
+          tupled2(fabc, fd) match {
+            case MyValid(((a, b, c), d)) => MyValid((a, b, c, d))
+            case MyInvalid(e) => MyInvalid(e)
+          }
+      }
+
+    def map4[E, A, B, C, D, X](t4: (MyValidated[E, A], MyValidated[E, B], MyValidated[E, C], MyValidated[E, D]))
+                              (f: (A, B, C, D) => X)
+                              (implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, X] =
+      tupled4(t4) match {
+        case MyValid(value) => MyValid(f(value._1, value._2, value._3, value._4))
+        case MyInvalid(err) => MyInvalid(err)
+      }
+
+    implicit class Tuple2Ops[E, A, B](t2: (MyValidated[E, A], MyValidated[E, B])) {
+      def tupled(implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, (A, B)] = tupled2(t2)
+      def mapN[C](f: (A, B) => C)(implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, C] = map2(t2)(f)
+    }
+
+    implicit class Tuple3Ops[E, A, B, C](t3: (MyValidated[E, A], MyValidated[E, B], MyValidated[E, C])) {
+      def tupled(implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, (A, B, C)] = tupled3(t3)
+      def mapN[D](f: (A, B, C) => D)(implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, D] = map3(t3)(f)
+    }
+
+    implicit class Tuple4Ops[E, A, B, C, D](t4: (MyValidated[E, A], MyValidated[E, B], MyValidated[E, C], MyValidated[E, D])) {
+      def tupled(implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, (A, B, C, D)] = tupled4(t4)
+      def mapN[X](f: (A, B, C, D) => X)(implicit S: MySemigroupal[MyValidated[E, ?]]): MyValidated[E, X] = map4(t4)(f)
+    }
+
+    implicit class ValidatedFromEither[E, A](either: Either[E, A]) {
+      def toValidated: MyValidated[E, A] = either match {
+        case Left(e)  => MyInvalid(e)
+        case Right(v) => MyValid(v)
+      }
     }
   }
 
--- cats/cats-scala/src/test/scala/Chapter6Tests.scala
+++ cats/cats-scala/src/test/scala/Chapter6Tests.scala
@@ -5,6 +5,8 @@ import Chapter6.SemigroupalInstances._
 import Types.{MyInvalid, MyValid, MyValidated}
 import Chapter6.ValidatedSyntax._
 
+import scala.util.{Failure, Success, Try}
+
 class Chapter6Tests extends FunSuite {
   test("Semigroupal instances") {
     val opt = MySemigroupal[Option].product(Some(1), Some(2))
@@ -51,6 +53,90 @@ class Chapter6Tests extends FunSuite {
 
     val rs4 = MySemigroupal[ListErrorsOr].product(el1, vl1)
     assert(rs4 === MyInvalid(List("Error 1")))
+  }
+
+  test("Semigroupal syntax") {
+    import Chapter2.MyMonoidInstance.stringMonoid
+    import Chapter2.MyMonoidInstance.listMonoid
+
+    val v1 = 1.valid[String]
+    val v2 = 2.valid[String]
+    val v3 = 3.valid[String]
+
+    val rs1 = (v1, v2).tupled
+    assert(rs1 === MyValid(1, 2))
+
+    val rs2 = (v1, v2, v3).tupled
+    assert(rs2 === MyValid(1, 2, 3))
+
+    val rs3 = (
+      List("Error 1").invalid[Int],
+      List("Error 2").invalid[Int],
+      List("Error 3").invalid[Int],
+      List("Error 4").invalid[Int]
+    ).tupled
+    assert(rs3 === MyInvalid(List("Error 1", "Error 2", "Error 3", "Error 4")))
+
+    val rs4 = (
+      3.valid[List[String]],
+      List("Error 1").invalid[Int],
+      List("Error 2").invalid[Int],
+      List("Error 3").invalid[Int]
+    ).tupled
+    assert(rs4 === MyInvalid(List("Error 1", "Error 2", "Error 3")))
+  }
+
+  test("Exercise: Form validation using Validated") {
+    import Chapter2.MyMonoidInstance.listMonoid
+    final case class User(name: String, age: Int, languages: List[String])
+    type Form = Map[String, String]
+    type FailFast[A] = Either[List[String], A]
+    type FailSlow[A] = MyValidated[List[String], A]
+
+    def getValue(form: Form, param: String): FailFast[String] =
+      form.get(param).toRight(List(s"$param field is not specified"))
+
+    def parseInt(name: String)(str: String): FailFast[Int] =
+      Try(str.toInt) match {
+        case Success(n) => Right(n)
+        case Failure(_) => Left(List(s"Cannot convert $name to an integer"))
+      }
+
+    def nonBlank(name: String)(str: String): FailFast[String] =
+      if (!str.isEmpty && str.exists(_ != 32)) Right(str)
+      else Left(List(s"$name must not be blanked"))
+
+    def nonNegative(name: String)(n: Int): FailFast[Int] =
+      if (n > 0) Right(n)
+      else Left(List(s"$name must be a positive integer"))
+
+    def readName(form: Form): FailFast[String] =
+      getValue(form, "name").flatMap(nonBlank("name"))
+
+    def readAge(form: Form): FailFast[Int] =
+      getValue(form, "age")
+        .flatMap(parseInt("age"))
+        .flatMap(nonNegative("age"))
+
+    def readLanguages(form: Form): FailFast[List[String]] =
+      getValue(form, "languages")
+        .flatMap(nonBlank("languages"))
+        .map(_.split(",").toList)
+
+    def getUser(form: Form): FailSlow[User] =
+      (
+        readName(form).toValidated,
+        readAge(form).toValidated,
+        readLanguages(form).toValidated
+      ).mapN(User.apply)
+
+    val form = Map[String, String]("name" -> "sondv", "age" -> "26", "languages" -> "en,vi" )
+    val user = getUser(form)
+    assert(user === MyValid(User("sondv", 26, List("en", "vi"))))
+
+    val form2 = Map[String, String]("name_" -> "sondv", "age" -> "0", "languages" -> "en,vi" )
+    val invalid = getUser(form2)
+    assert(invalid === MyInvalid(List("name field is not specified", "age must be a positive integer")))
   }
 
 }
